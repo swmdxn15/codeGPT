c266727c51f9f66e5eb0d5bed9e484dca957f37b
Swmdwn Khakhlary <swmdwnkhakhlary@Swmdwns-MacBook-Pro.local>
2022-12-29 01:32:01 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
     
     //fetch data from server - bot's response
 
-    const response = await fetch('http://localhost:5000',{
+    const response = await fetch('https://codegpt-3sk4.onrender.com',{
         method: 'POST',
         headers:{
             'Content-Type': 'application/json'
